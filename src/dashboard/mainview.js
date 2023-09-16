8ee7b96ee025a583d9b2c60d2900c62824d110f1
Miles Dahmann <mdahmann@gmail.com>
2023-09-16 09:04:17 -0500

    updating language on pricing section

--- src/dashboard/mainview.js
+++ src/dashboard/mainview.js
@@ -569,9 +569,9 @@ const MainViewer = () => {
               </div>
               <hr className="sentient-underline" />
               <h2 className="sentient-subtitle" style={{ fontWeight: '700' }}>Total Price: ${totalValue?.toLocaleString()}</h2>
-              <p className="sentient-content"> price includes tax + installation</p>
+              <p className="sentient-content"> Price includes tax, shipping from Italy, delivery, and installation.</p>
 
-              <h3 className="sentient-contenttitle pt-3"> <b>important:</b> Closet selections must be selected & purchased on <br /> Formsite: link to formsite</h3>
+              <h3 className="sentient-contenttitle pt-3"> <b>Important:</b> Closet selections must be selected & purchased on <a style={{ color: '#294734', fontWeight: '700' }} href="https://fs18.formsite.com/UrbanspaceLifestyle/The-Modern-Closets/index">Formsite</a>.</h3>
             </div>
           </div>
         </div>
