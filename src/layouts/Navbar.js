63e90f3f32ae95a13c8fe1531a1334d68ddbad07
Miles Dahmann <mdahmann@gmail.com>
2023-09-17 11:54:19 -0500

    navbar responsive updates

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -11,10 +11,8 @@ const NavbarPage = (props) => {
   const [visible, setVisible] = useState(false)
   return (
     <>
-      <CNavbar expand="md" colorScheme="light" className="px-3 d-flex justify-content-between m-0 p-0" style={{ height: '125px', backgroundColor: '#294734'}}>
-        <CContainer fluid >
+      <CNavbar expand="md" colorScheme="light" className="px-md-5 px-3 d-flex justify-content-between m-0 p-0" style={{ height: '125px', backgroundColor: '#294734'}}>
           <CNavbarBrand href="/" className="d-flex justify-content-start">
-            <CNavLink to="/" className="navbar-brand d-flex align-items-center" onClick={() => window.scrollTo(0, 0)}>
                <img
                 className="w-100"
                 src={LogoImage}
@@ -23,37 +21,21 @@ const NavbarPage = (props) => {
                   width: 'auto', height: 'auto', maxWidth: '100%', maxHeight: '100%',
                 }}
               />
-            </CNavLink> 
 
-            <div className="px-5 d-md-flex d-none">
+            <div className="px-lg-5 px-3 d-md-flex d-none">
               <h1 className="sentient-subtitle  d-flex align-items-center " style={{color: '#fafbfd'}}>CLOSET SELECTIONS <br /> EXPLORER </h1>
             </div>
-
           </CNavbarBrand>
-          <CNavbarToggler
-            aria-label="Toggle navigation"
-            aria-expanded={visible}
-            onClick={() => setVisible(!visible)}
-          />
-          <CCollapse className="navbar-collapse d-flex justify-content-end" visible={visible}>
-            <CNavbarNav>
-              <CNavItem >
-              <div className=" d-md-flex d-none">
-                <CNavLink href="https://fs18.formsite.com/UrbanspaceLifestyle/The-Modern-Closets/index" target="_blank" className="align-items-center" >
-                  <h2 className="sentient-subtitle " style={{ border: '1px solid #fafbfd', borderRadius: '16px', margin: '5px', padding: '10px', color: '#fafbfd' }}> ALREADY DECIDED? <br /> CLICK HERE TO PURCHASE </h2>
-                </CNavLink>
-              </div>
-              <div className="px-5 d-md-none d-flex">
-                {visible && 
-                  <CNavLink href="https://fs18.formsite.com/UrbanspaceLifestyle/The-Modern-Closets/index" target="_blank" className="align-items-center" >
-                    <h2 className="sentient-subtitle " style={{ border: '1px solid #fafbfd', borderRadius: '16px', margin: '5px', padding: '10px', color: '#000000' }}> ALREADY DECIDED? <br /> CLICK HERE TO PURCHASE </h2>
-                  </CNavLink> 
-                }
-              </div>                
-              </CNavItem>
-            </CNavbarNav>
-          </CCollapse>
-        </CContainer>
+          <div className="d-md-flex d-none ml-auto">
+              <a href="https://fs18.formsite.com/UrbanspaceLifestyle/The-Modern-Closets/index" target="_blank" className="navbar-brand d-flex align-items-center" onClick={() => window.scrollTo(0, 0)}>
+                <h2 className="sentient-subtitle d-flex align-items-center" style={{ border: '1px solid #fafbfd', borderRadius: '16px', margin: '5px', padding: '10px', color: '#fafbfd' }}> ALREADY DECIDED? <br /> CLICK HERE TO PURCHASE </h2>
+              </a>
+            </div>
+            <div className="d-md-none d-flex justify-content-end">
+              <a href="https://fs18.formsite.com/UrbanspaceLifestyle/The-Modern-Closets/index" target="_blank" className="navbar-brand d-flex align-items-center" onClick={() => window.scrollTo(0, 0)}>
+                <h2 className="sentient-subtitle d-flex align-items-center" style={{ border: '1px solid #fafbfd', borderRadius: '16px', margin: '5px', padding: '10px', color: '#fafbfd' }}> PURCHASE </h2>
+              </a>
+            </div>
       </CNavbar>
     </>
   )
