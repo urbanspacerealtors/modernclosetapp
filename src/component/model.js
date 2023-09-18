dabdfef398ab25da907d57e2a7e7b3d64d7058b4
Miles Dahmann <mdahmann@gmail.com>
2023-09-18 15:19:26 -0500

    updates to fix mtl issue

--- src/component/model.js
+++ src/component/model.js
@@ -6,20 +6,31 @@ const ObjModel = ({ objPath, mtlPath, rotation, largeOption }) => {
   const [loadedObject, setLoadedObject] = useState(null);
 
   useEffect(() => {
-    const mtlLoader = new MTLLoader();
-    mtlLoader.load(mtlPath, materials => {
-      materials.preload();
-      const objLoader = new OBJLoader();
-      objLoader.setMaterials(materials);
-      objLoader.load(objPath, (object) => {
-        object.traverse((child) => {
+    const loadMTL = new Promise((resolve, reject) => {
+      const mtlLoader = new MTLLoader();
+      mtlLoader.load(mtlPath, resolve, undefined, reject);
+    });
+
+    loadMTL
+      .then(materials => {
+        materials.preload();
+        return new Promise((resolve, reject) => {
+          const objLoader = new OBJLoader();
+          objLoader.setMaterials(materials);
+          objLoader.load(objPath, resolve, undefined, reject);
+        });
+      })
+      .then(object => {
+        object.traverse(child => {
           if (child.isMesh && largeOption) {
             child.material.side = 2;
           }
         });
         setLoadedObject(object);
+      })
+      .catch(error => {
+        console.error('An error occurred:', error);
       });
-    });
   }, [mtlPath, objPath, largeOption]);
 
   return (
