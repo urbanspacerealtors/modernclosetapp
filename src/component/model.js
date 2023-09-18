653628889ce7053513fd304547c4a7e075665bd0
Miles Dahmann <mdahmann@gmail.com>
2023-09-18 14:26:01 -0500

    update to fix issue with MTL file

--- src/component/model.js
+++ src/component/model.js
@@ -1,31 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
-import { useRef } from "react";
-import { useLoader  } from "react-three-fiber";
-// import { MTLLoader, OBJLoader } from 'three-stdlib';
 
 const ObjModel = ({ objPath, mtlPath, rotation, largeOption }) => {
-  const modelRef = useRef();
-  const obj = useLoader(OBJLoader, objPath, loader => {
+  const [loadedObject, setLoadedObject] = useState(null);
+
+  useEffect(() => {
     const mtlLoader = new MTLLoader();
     mtlLoader.load(mtlPath, materials => {
-      loader.setMaterials(materials);
-      loader.load(objPath, (object) => {
+      materials.preload();
+      const objLoader = new OBJLoader();
+      objLoader.setMaterials(materials);
+      objLoader.load(objPath, (object) => {
         object.traverse((child) => {
           if (child.isMesh && largeOption) {
             child.material.side = 2;
           }
         });
-      });  
+        setLoadedObject(object);
+      });
     });
-  });
+  }, [mtlPath, objPath, largeOption]);
 
   return (
-    <group rotation={rotation} ref={modelRef}>
-      <primitive object={obj} />
+    <group rotation={rotation}>
+      {loadedObject ? <primitive object={loadedObject} /> : null}
     </group>
   );
 };
 
 export default ObjModel;
-  
