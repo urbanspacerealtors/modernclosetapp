510d425ecea885eff0bf068cd1db4c2671293495
Miles Dahmann <mdahmann@gmail.com>
2023-09-18 14:44:06 -0500

    Update model.js

--- src/component/model.js
+++ src/component/model.js
@@ -1,47 +1,31 @@
+import React, { useEffect, useState } from 'react';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
-import { useRef, useState, useEffect } from "react";
-import { useLoader, useFrame } from "react-three-fiber";
 
 const ObjModel = ({ objPath, mtlPath, rotation, largeOption }) => {
-  const modelRef = useRef();
-  const [isLoading, setIsLoading] = useState(true);
-  const obj = useLoader(OBJLoader, objPath, loader => {
-    setIsLoading(true);
+  const [loadedObject, setLoadedObject] = useState(null);
+
+  useEffect(() => {
     const mtlLoader = new MTLLoader();
     mtlLoader.load(mtlPath, materials => {
-      loader.setMaterials(materials);
-      loader.load(objPath, (object) => {
+      materials.preload();
+      const objLoader = new OBJLoader();
+      objLoader.setMaterials(materials);
+      objLoader.load(objPath, (object) => {
         object.traverse((child) => {
           if (child.isMesh && largeOption) {
             child.material.side = 2;
           }
         });
-        setIsLoading(false);
-      });  
+        setLoadedObject(object);
+      });
     });
-  });
-
-  useEffect(() => {
-    if (obj) {
-      setIsLoading(false);
-    }
-  }, [obj]);
+  }, [mtlPath, objPath, largeOption]);
 
   return (
-    <>
-      {isLoading ? (
-        <mesh>
-          {/* Replace this with your loading mesh or other indicator */}
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
-      ) : (
-        <group rotation={rotation} ref={modelRef}>
-          <primitive object={obj} />
-        </group>
-      )}
-    </>
+    <group rotation={rotation}>
+      {loadedObject ? <primitive object={loadedObject} /> : null}
+    </group>
   );
 };
 
