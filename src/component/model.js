df492e5efb2a94a8530bc0fb47bf2c1ba79a4940
Miles Dahmann <mdahmann@gmail.com>
2023-09-18 14:38:35 -0500

    adding loader spinner

--- src/component/model.js
+++ src/component/model.js
@@ -1,33 +1,48 @@
-import React, { useEffect, useState } from 'react';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import { useRef, useState, useEffect } from "react";
+import { useLoader, useFrame } from "react-three-fiber";
 
 const ObjModel = ({ objPath, mtlPath, rotation, largeOption }) => {
-  const [loadedObject, setLoadedObject] = useState(null);
-
-  useEffect(() => {
+  const modelRef = useRef();
+  const [isLoading, setIsLoading] = useState(true);
+  const obj = useLoader(OBJLoader, objPath, loader => {
+    setIsLoading(true);
     const mtlLoader = new MTLLoader();
     mtlLoader.load(mtlPath, materials => {
-      materials.preload();
-      const objLoader = new OBJLoader();
-      objLoader.setMaterials(materials);
-      objLoader.load(objPath, (object) => {
+      loader.setMaterials(materials);
+      loader.load(objPath, (object) => {
         object.traverse((child) => {
           if (child.isMesh && largeOption) {
             child.material.side = 2;
           }
         });
-        setLoadedObject(object);
-      });
+        setIsLoading(false);
+      });  
     });
-  }, [mtlPath, objPath, largeOption]);
+  });
+
+  useEffect(() => {
+    if (obj) {
+      setIsLoading(false);
+    }
+  }, [obj]);
 
   return (
-    <group rotation={rotation}>
-      {loadedObject ? <primitive object={loadedObject} /> : null}
-    </group>
+    <>
+      {isLoading ? (
+        <mesh>
+          {/* Replace this with your loading mesh or other indicator */}
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+      ) : (
+        <group rotation={rotation} ref={modelRef}>
+          <primitive object={obj} />
+        </group>
+      )}
+    </>
   );
 };
-
 
 export default ObjModel;
